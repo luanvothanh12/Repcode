c04e2f276859f6583172dac078a109fb411e85cc
Hussain Ali <HussainAli22422@gmail.com>
2024-02-02 16:33:45 -0800

    dummy redeploy

--- src/pages/api/createCollection.tsx
+++ src/pages/api/createCollection.tsx
@@ -36,4 +36,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
+
 }
