36d8941a00af4f2d1ab4eb9a72fbebb24517d4b2
Hussain Ali <HussainAli22422@gmail.com>
2024-09-29 19:46:56 -0700

    SEO stuff

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,22 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
           <link rel="icon" href="/favicon.ico" />
+          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+          <script type="application/ld+json">
+            {JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              "name": "Repcode",
+              "url": "https://www.repcode.io",
+              "description": "Your personalized platform to master Leetcode. Organize problems, review via spatial repetition, and get AI-powered feedback.",
+              "potentialAction": {
+                "@type": "SearchAction",
+                "target": "https://www.repcode.io/search?q={search_term_string}",
+                "query-input": "required name=search_term_string"
+              }
+            })}
+          </script>
         </Head>
         <body>
           <Main />
