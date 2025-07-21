fbeea6a74202905c90735081a6c998b8b470ae46
Omkar Sathish <omkarsathish06@gmail.com>
2025-07-21 08:58:30 +0530

    feature: landing-page github star

---
+++ src/pages/api/github-stars.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const response = await fetch(
+    "https://api.github.com/repos/hussiiii/repcode",
+    {
+      headers: {
+        Accept: "application/vnd.github+json",
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    return res.status(500).json({ stars: 0 });
+  }
+
+  const data = await response.json();
+  return res.status(200).json({ stars: data.stargazers_count });
+}
