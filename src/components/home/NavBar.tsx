ea19e28c8c82f2071f7a19ef67b0c09f45d7a69c
Hussain Ali <HussainAli22422@gmail.com>
2025-09-01 13:57:16 -0700

    updates

--- src/components/home/NavBar.tsx
+++ src/components/home/NavBar.tsx
@@ -96,7 +96,7 @@ const NavBar = () => {
                   </div>
                   
                   {/* Version text */}
-                  <span className="text-xs font-medium text-[#E5E7EB]">v2.8</span>
+                  <span className="text-xs font-medium text-[#E5E7EB]">v2.9</span>
                 </div>
               </div>
             </div>
